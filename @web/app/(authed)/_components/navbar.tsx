4d3ed0d0b4570ae5418006a3c75267b2fec1c2d9
Din <dinsterizer@gmail.com>
2023-12-08 20:20:15 +0700

    wip

--- @web/app/(authed)/_components/navbar.tsx
+++ @web/app/(authed)/_components/navbar.tsx
@@ -4,39 +4,97 @@ import { Button } from '@dinstack/ui/button'
 import { ScrollArea } from '@dinstack/ui/scroll-area'
 import { Skeleton } from '@dinstack/ui/skeleton'
 import { DashboardIcon } from '@radix-ui/react-icons'
+import { match } from 'ts-pattern'
 
-export function Navbar() {
+type Props = {
+  size?: 'default' | 'icon'
+}
+
+const menuItems = [
+  {
+    Icon: DashboardIcon,
+    label: 'Dashboard',
+  },
+  {
+    Icon: DashboardIcon,
+    label: 'Dashboard2',
+  },
+  {
+    Icon: DashboardIcon,
+    label: 'Dashboard3',
+  },
+  {
+    Icon: DashboardIcon,
+    label: 'Dashboard4',
+  },
+  {
+    Icon: DashboardIcon,
+    label: 'Dashboard5',
+  },
+]
+
+export function Navbar({ size = 'default' }: Props) {
   return (
     <div className="flex flex-col h-full">
-      <Button type="button" variant={'ghost'} size={'lg'} className="w-full justify-start px-3">
-        <Skeleton className="h-8 w-8 mr-3" />
-        <Skeleton className="h-6 w-36" />
-      </Button>
+      {match(size)
+        .with('default', () => (
+          <Button type="button" variant={'ghost'} size={'lg'} className="w-full justify-start px-3">
+            <Skeleton className="h-8 w-8 mr-3" />
+            <Skeleton className="h-6 w-36" />
+          </Button>
+        ))
+        .with('icon', () => (
+          <Button type="button" variant={'ghost'} size={'icon'}>
+            <Skeleton className="h-8 w-8" />
+          </Button>
+        ))
+        .exhaustive()}
 
       <ScrollArea className="flex-1 mt-8">
-        <div className="space-y-4">
-          <Button type="button" variant={'ghost'} className="w-full justify-start px-3">
-            <DashboardIcon className="h-4 w-4 mr-3" />
-            Dashboard
-          </Button>
-          <Button type="button" variant={'ghost'} className="w-full justify-start px-3">
-            <DashboardIcon className="h-4 w-4 mr-3" />
-            Dashboard
-          </Button>
-          <Button type="button" variant={'ghost'} className="w-full justify-start px-3">
-            <DashboardIcon className="h-4 w-4 mr-3" />
-            Dashboard
-          </Button>
+        <div className="flex flex-col gap-4">
+          {match(size)
+            .with('default', () =>
+              menuItems.map((item) => (
+                <Button key={item.label} type="button" variant={'ghost'} className="w-full justify-start px-3">
+                  <item.Icon className="h-4 w-4 mr-3" />
+                  {item.label}
+                </Button>
+              )),
+            )
+            .with('icon', () =>
+              menuItems.map((item) => (
+                <Button key={item.label} type="button" variant={'ghost'} size={'icon'}>
+                  <item.Icon className="h-4 w-4" />
+                </Button>
+              )),
+            )
+            .exhaustive()}
         </div>
       </ScrollArea>
 
-      <div className="space-y-4">
-        <Skeleton className="h-6 w-full" />
+      <div className="flex flex-col gap-4">
+        {match(size)
+          .with('default', () => <Skeleton className="h-6 w-full" />)
+          .with('icon', () => (
+            <Button type="button" variant={'ghost'} size={'icon'}>
+              <Skeleton className="h-6 w-6" />
+            </Button>
+          ))
+          .exhaustive()}
 
-        <Button type="button" variant={'ghost'} size={'lg'} className="w-full justify-start px-3">
-          <Skeleton className="h-8 w-8 mr-3 rounded-full" />
-          <Skeleton className="h-6 w-36" />
-        </Button>
+        {match(size)
+          .with('default', () => (
+            <Button type="button" variant={'ghost'} size={'lg'} className="w-full justify-start px-3">
+              <Skeleton className="h-8 w-8 mr-3 rounded-full" />
+              <Skeleton className="h-6 w-36" />
+            </Button>
+          ))
+          .with('icon', () => (
+            <Button type="button" variant={'ghost'} size={'icon'}>
+              <Skeleton className="h-8 w-8" />
+            </Button>
+          ))
+          .exhaustive()}
       </div>
     </div>
   )
--- @web/app/(authed)/layout.tsx
+++ @web/app/(authed)/layout.tsx
@@ -1,19 +1,46 @@
+'use client'
+
 import { Button } from '@dinstack/ui/button'
-import { CaretLeftIcon } from '@radix-ui/react-icons'
+import { CaretLeftIcon, CaretRightIcon } from '@radix-ui/react-icons'
+import { useState } from 'react'
+import { match } from 'ts-pattern'
 import { Navbar } from './_components/navbar'
 import { RequireAuthedWrapper } from './_wrappers/require-authed'
 
 export default function AuthedLayout({ children }: { children: React.ReactNode }) {
+  const [sidebarSize, setSidebarSize] = useState<'default' | 'icon'>('default')
+
   return (
     <RequireAuthedWrapper>
       <div className="h-full flex">
         <div className="h-full max-w-[280px] p-4 pb-6 relative z-10">
-          <Navbar />
+          <Navbar size={sidebarSize} />
 
           <div className="absolute top-2 -right-3">
-            <Button type="button" variant={'outline'} size={'icon'} className="h-6 w-6 bg-background">
-              <CaretLeftIcon className="h-4 w-4" />
-            </Button>
+            {match(sidebarSize)
+              .with('default', () => (
+                <Button
+                  type="button"
+                  variant={'outline'}
+                  size={'icon'}
+                  className="h-6 w-6 bg-background"
+                  onClick={() => setSidebarSize('icon')}
+                >
+                  <CaretLeftIcon className="h-4 w-4" />
+                </Button>
+              ))
+              .with('icon', () => (
+                <Button
+                  type="button"
+                  variant={'outline'}
+                  size={'icon'}
+                  className="h-6 w-6 bg-background"
+                  onClick={() => setSidebarSize('default')}
+                >
+                  <CaretRightIcon className="h-4 w-4" />
+                </Button>
+              ))
+              .exhaustive()}
           </div>
         </div>
         <div className="flex-1 h-full">{children}</div>
