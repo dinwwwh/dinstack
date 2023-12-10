de2eeac031db301a95e453a1169598d575b441f7
Din <dinsterizer@gmail.com>
2023-12-10 15:16:10 +0700

    fix

--- @web/app/_providers/stores.tsx
+++ @web/app/_providers/stores.tsx
@@ -2,11 +2,10 @@
 
 import { useAuthStore } from '@web/stores/auth'
 import { useHistoryStore } from '@web/stores/history'
-import { useEffect } from 'react'
+import { useMemo } from 'react'
 
 export function StoresProvider({ children }: { children: React.ReactNode }) {
-  // TODO: fix when using store inside another useEffect
-  useEffect(() => {
+  useMemo(() => {
     useAuthStore.persist.rehydrate()
     useHistoryStore.persist.rehydrate()
   }, [])
