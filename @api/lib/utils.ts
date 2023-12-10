5334f24fde46f73856bcbdf23323cbd5cd111784
Din <dinsterizer@gmail.com>
2023-12-10 19:22:55 +0700

    create organization and more

--- @api/lib/utils.ts
+++ @api/lib/utils.ts
@@ -6,3 +6,7 @@ export function generateFallbackAvatarUrl(user: { name: string; email: string })
 
   return avatarUrl.toString()
 }
+
+export function generateFallbackLogoUrl(organization: { name: string }) {
+  return `https://ui-avatars.com/api/${organization.name}/96/f4f4f5/09090b/1`
+}
---
+++ @api/routes/organization/create.ts
@@ -0,0 +1,55 @@
+import { OrganizationMembers, Organizations } from '@api/database/schema'
+import { generateFallbackLogoUrl } from '@api/lib/utils'
+import { authedProcedure } from '@api/trpc'
+import { TRPCError } from '@trpc/server'
+import { z } from 'zod'
+
+export const organizationCreateRoute = authedProcedure
+  .input(
+    z.object({
+      organization: z.object({
+        name: z.string(),
+      }),
+    }),
+  )
+  .mutation(async ({ ctx, input }) => {
+    // TODO: limit
+
+    const { organization, organizationMember } = await ctx.db.transaction(async (trx) => {
+      const [organization] = await trx
+        .insert(Organizations)
+        .values({
+          name: input.organization.name,
+          logoUrl: generateFallbackLogoUrl({ name: input.organization.name }),
+        })
+        .returning()
+
+      if (!organization) {
+        throw new TRPCError({
+          code: 'INTERNAL_SERVER_ERROR',
+          message: 'Failed to create organization',
+        })
+      }
+
+      const [organizationMember] = await trx
+        .insert(OrganizationMembers)
+        .values({
+          organizationId: organization.id,
+          userId: ctx.auth.user.id,
+          role: 'admin',
+        })
+        .returning()
+
+      return {
+        organization,
+        organizationMember,
+      }
+    })
+
+    return {
+      organization: {
+        ...organization,
+        members: [organizationMember],
+      },
+    }
+  })
