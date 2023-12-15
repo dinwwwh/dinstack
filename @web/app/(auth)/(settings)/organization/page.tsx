800b485fcccfa7f265410bd706b5ef1de4fb401d
Din <dinsterizer@gmail.com>
2023-12-15 15:02:37 +0700

    navigation

---
+++ @web/app/(auth)/(settings)/organization/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Organization',
+}
+
+export default function OrganizationPage() {
+  return (
+    <main className="p-4">
+      <div className="mx-auto max-w-7xl divide-y">organization</div>
+    </main>
+  )
+}
