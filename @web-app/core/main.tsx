3f3fd98d4d2a8387c5ac0dc81d27ead1f8dc92ce
Din <dinsterizer@gmail.com>
2023-12-24 09:13:52 +0700

    wip

---
+++ @web-app/core/main.tsx
@@ -0,0 +1,10 @@
+import '+ui/styles/globals.css'
+import { Button } from '+ui/ui/button'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Button>xin chao</Button>
+  </React.StrictMode>,
+)
---
+++ @web-app/vite.config.ts
@@ -0,0 +1,15 @@
+import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@web-app': resolve(__dirname, './'),
+      '+ui': resolve(__dirname, '../+ui'),
+      '+ui/_/../ui/*': resolve(__dirname, '../+ui/ui'),
+    },
+  },
+})
