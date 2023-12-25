0cee1bfd19564a1be2ec5d16d3db9d5480609d8b
Din <dinsterizer@gmail.com>
2023-12-25 15:45:22 +0700

    new web app

---
+++ apps/web/tailwind.config.ts
@@ -0,0 +1,17 @@
+import baseConfig from '@dinstack/ui/configs/tailwind.config'
+import type { Config } from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+export default {
+  ...baseConfig,
+  theme: {
+    ...baseConfig.theme,
+    extend: {
+      ...baseConfig.theme.extend,
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        title: ['Space Grotesk', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+} satisfies Config
--- packages/ui/configs/tailwind.config.ts
+++ packages/ui/configs/tailwind.config.ts
@@ -12,6 +12,7 @@ export default {
     './providers/**/*.*',
     './stores/**/*.*',
     './lib/**/*.*',
+    './core/**/*.*',
     resolve(__dirname, '../ui/**/*.*'),
   ],
   theme: {
