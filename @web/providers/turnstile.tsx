ddfe908d9679e6aadf29efa3589dd56ecde8be15
Din <dinsterizer@gmail.com>
2023-12-30 16:08:54 +0700

    fix turnstile display

--- @web/providers/turnstile.tsx
+++ @web/providers/turnstile.tsx
@@ -16,43 +16,41 @@ export function TurnstileProvider(props: { children: React.ReactNode }) {
   return (
     <>
       {props.children}
-      <Portal.Root>
-        <div
-          className={cn(
-            'fixed top-0 left-0 right-0 bottom-0 z-[99999] flex items-center justify-center bg-background/80',
-            {
-              'top-[100%] bottom-[-100%]': !turnstileStore.isShowChallenge,
-            },
-          )}
-        >
-          <Turnstile
-            ref={turnstileRef}
-            id={id}
-            siteKey={env.TURNSTILE_SITE_KEY}
-            options={{
-              theme: systemStore.theme === 'system' ? 'auto' : systemStore.theme,
-            }}
-            onSuccess={(token) => {
-              useTurnstileStore.setState(() => ({
-                token,
-                instance: turnstileRef.current,
-              }))
-            }}
-            onError={() => {
-              useTurnstileStore.setState(() => ({
-                token: null,
-                instance: turnstileRef.current,
-              }))
-            }}
-            onExpire={() => {
-              useTurnstileStore.setState(() => ({
-                token: null,
-                instance: turnstileRef.current,
-              }))
-              turnstileRef.current?.reset()
-            }}
-          />
-        </div>
+      <Portal.Root
+        className={cn(
+          'pointer-events-auto fixed top-0 left-0 right-0 bottom-0 z-[99999] flex items-center justify-center bg-background/80',
+          {
+            'top-[100%] bottom-[-100%]': !turnstileStore.isShowChallenge,
+          },
+        )}
+      >
+        <Turnstile
+          ref={turnstileRef}
+          id={id}
+          siteKey={env.TURNSTILE_SITE_KEY}
+          options={{
+            theme: systemStore.theme === 'system' ? 'auto' : systemStore.theme,
+          }}
+          onSuccess={(token) => {
+            useTurnstileStore.setState(() => ({
+              token,
+              instance: turnstileRef.current,
+            }))
+          }}
+          onError={() => {
+            useTurnstileStore.setState(() => ({
+              token: null,
+              instance: turnstileRef.current,
+            }))
+          }}
+          onExpire={() => {
+            useTurnstileStore.setState(() => ({
+              token: null,
+              instance: turnstileRef.current,
+            }))
+            turnstileRef.current?.reset()
+          }}
+        />
       </Portal.Root>
     </>
   )
