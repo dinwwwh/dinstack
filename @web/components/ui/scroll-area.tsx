0fb6376dbbdc90f55e440244e89a12c15f44cdfc
Din <dinsterizer@gmail.com>
2024-02-18 20:08:08 +0700

    fix: scroll-area z index problem

--- @web/components/ui/scroll-area.tsx
+++ @web/components/ui/scroll-area.tsx
@@ -28,7 +28,7 @@ const ScrollBar = React.forwardRef<
     ref={ref}
     orientation={orientation}
     className={cn(
-      'flex touch-none select-none transition-colors',
+      'z-[999] flex touch-none select-none transition-colors',
       orientation === 'vertical' &&
         'h-full w-1.5 @2xl:hover:w-2 @6xl:hover:w-2.5 border-l border-l-transparent p-[1px]',
       orientation === 'horizontal' &&
